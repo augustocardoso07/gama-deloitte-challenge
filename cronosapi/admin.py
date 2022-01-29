51755f4204fc60640d6a1d7d874b2e423898b62f
Renan Cardoso <renan.cardoso@going2.com.br>
2022-01-29 19:46:49 -0300

    models and rest_framework

--- cronosapi/admin.py
+++ cronosapi/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Member, Post, Service
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(Post)
+admin.site.register(Service)
--- cronosapi/models.py
+++ cronosapi/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    body = models.TextField()
+    author = models.ForeignKey('Member', on_delete=models.DO_NOTHING)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
