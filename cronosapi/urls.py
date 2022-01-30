a57cdf7eda03469c12f96b2548afb32655d9b058
Renan Cardoso <renan.cardoso@going2.com.br>
2022-01-29 22:31:15 -0300

    Docs and Auth

--- cronosapi/urls.py
+++ cronosapi/urls.py
@@ -1,12 +1,21 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
 router = routers.DefaultRouter()
 router.register('members', views.MemberView)
 router.register('posts', views.PostView)
 router.register('services', views.ServiceView)
 
-urlpatterns = [
-    path('', include(router.urls))
+
+apipatters = [
+    path('', include(router.urls)),
+]
+
+schema = get_swagger_view(
+    title="Documentação da Cronos API", url='/docs', patterns=apipatters)
+
+urlpatterns = apipatters + [
+    re_path('docs', schema),
 ]
--- cronosapi/views.py
+++ cronosapi/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Member, Post, Service
 from .serializers import MemberSerializer, PostSerializer, ServiceSerializer
 
@@ -7,13 +10,19 @@ from .serializers import MemberSerializer, PostSerializer, ServiceSerializer
 class MemberView(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class PostView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class ServiceView(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
