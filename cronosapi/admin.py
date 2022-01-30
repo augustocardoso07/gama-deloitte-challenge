fe1a7fad8e71f72c297fcb5c973c540b6507ac3a
Renan Cardoso <renan.cardoso@going2.com.br>
2022-01-30 11:41:43 -0300

    unregister Group and User

--- cronosapi/admin.py
+++ cronosapi/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Member, Post, Service
+from django.contrib.auth.models import Group, User
 
 admin.site.register(Member)
 admin.site.register(Post)
 admin.site.register(Service)
+admin.site.unregister(Group)
+admin.site.unregister(User)
